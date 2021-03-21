093aa4ee2ffef6d161825963782fe4193deaacc7
Michael Peters <michaelpeters5647@gmail.com>
2021-03-21 14:29:51 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+class Neuron {
+  constructor(activation) {
+    this.activation = activation;
+  }
+}
+
+const data = [
+  [new Neuron(4), new Neuron(4), new Neuron(4), new Neuron(4), new Neuron(4)],
+  [new Neuron(4), new Neuron(4), new Neuron(4), new Neuron(4), new Neuron(4)],
+  [new Neuron(4), new Neuron(4), new Neuron(4), new Neuron(4), new Neuron(4)],
+  [new Neuron(4), new Neuron(4), new Neuron(4), new Neuron(4), new Neuron(4)],
+  [new Neuron(4), new Neuron(4), new Neuron(4), new Neuron(4), new Neuron(4)],
+  [new Neuron(4), new Neuron(4), new Neuron(4), new Neuron(4), new Neuron(4)],
+  [new Neuron(4), new Neuron(4), new Neuron(4), new Neuron(4), new Neuron(4)],
+  [new Neuron(4), new Neuron(4), new Neuron(4), new Neuron(4), new Neuron(4)],
+  [new Neuron(4), new Neuron(4), new Neuron(4), new Neuron(4), new Neuron(4)],
+  [new Neuron(4), new Neuron(4), new Neuron(4), new Neuron(4), new Neuron(4)],
+  [new Neuron(4), new Neuron(4), new Neuron(4), new Neuron(4), new Neuron(4)],
+  [new Neuron(4), new Neuron(4), new Neuron(4), new Neuron(4), new Neuron(4)],
+  [new Neuron(4), new Neuron(4), new Neuron(4), new Neuron(4), new Neuron(4)],
+  [new Neuron(4), new Neuron(4), new Neuron(4), new Neuron(4), new Neuron(4)],
+  [new Neuron(4), new Neuron(4), new Neuron(4), new Neuron(4), new Neuron(4)],
+  [new Neuron(4), new Neuron(4), new Neuron(4), new Neuron(4), new Neuron(4)],
+  [new Neuron(4), new Neuron(4), new Neuron(4), new Neuron(4), new Neuron(4)],
+  [new Neuron(4), new Neuron(4), new Neuron(4), new Neuron(4), new Neuron(4)],
+  [new Neuron(4), new Neuron(4), new Neuron(4), new Neuron(4), new Neuron(4)],
+  [new Neuron(4), new Neuron(4), new Neuron(4), new Neuron(4), new Neuron(4)],
+  [new Neuron(4), new Neuron(4), new Neuron(4), new Neuron(4), new Neuron(4)],
+  [new Neuron(4), new Neuron(4), new Neuron(4), new Neuron(4), new Neuron(4)],
+  [new Neuron(4), new Neuron(4), new Neuron(4), new Neuron(4), new Neuron(4)],
+  [new Neuron(4), new Neuron(4), new Neuron(4), new Neuron(4), new Neuron(4)],
+  [new Neuron(4), new Neuron(4), new Neuron(4), new Neuron(4), new Neuron(4)],
+  [new Neuron(4), new Neuron(4), new Neuron(4), new Neuron(4), new Neuron(4)],
+  [new Neuron(4), new Neuron(4), new Neuron(4), new Neuron(4), new Neuron(4)],
+  [new Neuron(4), new Neuron(4), new Neuron(4), new Neuron(4), new Neuron(4)],
+];
+
+console.log(data)
